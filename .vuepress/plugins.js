2be11b72bb375934e407745332bbf04aae9b70e1
yzhe819 <yzhe819@aucklanduni.ac.nz>
2021-07-04 16:18:36 +1200

    push

--- .vuepress/plugins.js
+++ .vuepress/plugins.js
@@ -2,11 +2,10 @@ module.exports = [
     [
         "@vuepress-reco/vuepress-plugin-rss",
         {
-            site_url: "https://yzhe819.github.io/", //网站地址
-            copyright: "Tsanfer", //版权署名
+            site_url: "https://yzhe819.github.io", //网站地址
+            copyright: "yzhe819", //版权署名
             name: "yzhe819", // 使用名称
             desc: "yzhe819's Blog", // 描述
-            repo: 'yzhe819.github.io', // Github 项目名称
             feature: 'feature', // 类型：style（优化主题效果），feature（扩展主题功能）
             scenes: 'independent' // 使用场景：develop（为主题开发提供组件或方法），independent（单纯扩展主题功能）
         }
